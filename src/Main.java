30d1b48d5c4fc67d210c5cebd000185452006a6e
Livenskaya Olga <livenskaya@list.ru>
2023-07-22 00:31:24 +0300

    Add program

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        int prTicket = 20000;
+        int prMili = 20;
+        int bonus = prTicket / prMili;
+
+        System.out.println("Стоимость билета: " + prTicket + " руб.");
+        System.out.println("Начисленнвх бонусов: " + bonus + " миль");
+
     }
 }
